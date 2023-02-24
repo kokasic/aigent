edca2324c9074bd916611902f40e7281fc4d6e56
Kshitij Kokas <kshitijkokas@Kshitijs-MacBook-Air.local>
2023-02-24 22:12:32 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5500', {
+    const response = await fetch('https://aigent.onrender.com/', {
         method: 'POST',
         headers: { 
             'Content-Type': 'application/json',
